1096c3a6f4187617021d8ab28988557d3eba1033
Maru Di Primio <mariana.diprimio@gmail.com>
2023-10-20 11:08:11 +0200

    upload project

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn_menu = document.querySelector(".menu-icon");
+const menu_container = document.querySelector(".menu-mobile");
+
+btn_menu.addEventListener('click',()=>{
+    menu_container.classList.toggle('active');
+    btn_menu.classList.toggle('opened');
+})
